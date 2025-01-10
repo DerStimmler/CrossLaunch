9a7e688bc282ff52312b75b25f18bcc5e1caefc8
Tim Schneider <43130816+DerStimmler@users.noreply.github.com>
2025-01-10 17:44:44 +0100

    feat: add xbox controller support

---
+++ CrossLaunch/Features/Gamepad/GamepadHandler.cs
@@ -0,0 +1,45 @@
+﻿using System.Threading;
+using System.Threading.Tasks;
+using WindowsInput;
+using WindowsInput.Native;
+
+namespace CrossLaunch.Features.Gamepad;
+
+public class GamepadHandler
+{
+  private static readonly KeyboardSimulator KeyboardSimulator = new(new InputSimulator());
+
+  public static void HandleGamepadInput(CancellationToken ct)
+  {
+    var gamepadInput = new GamepadInput();
+
+    gamepadInput.ButtonPressed += direction =>
+    {
+      SimulateArrowKeyPress(direction);
+    };
+
+    gamepadInput.StartPollingAsync(ct);
+  }
+
+  private static void SimulateArrowKeyPress(string direction)
+  {
+    switch (direction)
+    {
+      case "Up":
+        KeyboardSimulator.KeyPress(VirtualKeyCode.UP);
+        break;
+      case "Down":
+        KeyboardSimulator.KeyPress(VirtualKeyCode.DOWN);
+        break;
+      case "Left":
+        KeyboardSimulator.KeyPress(VirtualKeyCode.LEFT);
+        break;
+      case "Right":
+        KeyboardSimulator.KeyPress(VirtualKeyCode.RIGHT);
+        break;
+      case "A":
+        KeyboardSimulator.KeyPress(VirtualKeyCode.RETURN);
+        break;
+    }
+  }
+}
---
+++ CrossLaunch/Features/Gamepad/GamepadInput.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+using SharpDX.XInput;
+
+namespace CrossLaunch.Features.Gamepad;
+
+public class GamepadInput
+{
+  private readonly Controller _controller = new(UserIndex.One);
+  private GamepadButtonFlags _previousButtons; //For fixing holding the button down
+  private bool _isRunning;
+  private readonly int _pollingInterval;
+
+  public GamepadInput(int pollingInterval = 50)
+  {
+    _pollingInterval = pollingInterval;
+  }
+
+  public event Action<string> ButtonPressed = null!;
+
+  public void StartPollingAsync(CancellationToken ct)
+  {
+    _isRunning = true;
+
+    Task.Run(
+      async () =>
+      {
+        while (_isRunning && !ct.IsCancellationRequested)
+        {
+          UpdateState();
+          await Task.Delay(_pollingInterval, ct);
+        }
+
+        StopPolling();
+      },
+      ct
+    );
+  }
+
+  public void StopPolling()
+  {
+    _isRunning = false;
+  }
+
+  private void UpdateState()
+  {
+    if (!_controller.IsConnected)
+      return;
+
+    var state = _controller.GetState();
+    var buttons = state.Gamepad.Buttons;
+
+    if (buttons == _previousButtons)
+      return;
+
+    if (buttons.HasFlag(GamepadButtonFlags.DPadUp))
+      ButtonPressed("Up");
+    else if (buttons.HasFlag(GamepadButtonFlags.DPadDown))
+      ButtonPressed("Down");
+    else if (buttons.HasFlag(GamepadButtonFlags.DPadLeft))
+      ButtonPressed("Left");
+    else if (buttons.HasFlag(GamepadButtonFlags.DPadRight))
+      ButtonPressed("Right");
+    else if (buttons.HasFlag(GamepadButtonFlags.A) && !_previousButtons.HasFlag(GamepadButtonFlags.A))
+      ButtonPressed("A");
+
+    _previousButtons = buttons;
+  }
+}
